6b914a1d330659d276b4d25bcead4c7785ce501e
Pepto <monsieurfranci@gmail.com>
2023-11-08 14:31:48 +0100

    added all elem & styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import { useState } from 'react'
-
+import Die from "./Die.jsx"
 
 function App() {
   return (
-    <p>hello</p>
+    <main>
+      <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+            <div className="dice-container">
+                <Die n="1" />
+                <Die n="2" />
+                <Die n="6" />
+                <Die n="5" />
+                <Die n="4" />
+                <Die n="2" />
+                <Die n="2" />
+                <Die n="3" />
+                <Die n="2" />
+                <Die n="5" />  
+            </div>
+            <button className="roll-dice">Roll</button>
+        </main>
   )
 }
 
---
+++ src/Die.jsx
@@ -0,0 +1,8 @@
+
+export default function Die(props) {
+    return (
+        <div className="die-face">
+            <h2 className="die-num">{props.n}</h2>
+        </div>
+    )
+}
