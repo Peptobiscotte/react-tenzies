645d09f536f203a1a4f1142e20c15f21b9deebb0
Pepto <monsieurfranci@gmail.com>
2023-11-08 14:42:33 +0100

    added roll to generate numbers

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,31 @@ import { useState } from 'react'
 import Die from "./Die.jsx"
 
 function App() {
+
+  const [dice, setDice] = useState(allNewDice())
+    
+  function allNewDice() {
+      const newDice = []
+      for (let i = 0; i < 10; i++) {
+          newDice.push(Math.ceil(Math.random() * 6))
+      }
+      return newDice
+  }
+  
+  function rollDice() {
+      setDice(allNewDice())
+  }
+  
+  const diceElements = dice.map((die, i) => <Die n={die} key={i}/>)
+
   return (
     <main>
       <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
-                <Die n="1" />
-                <Die n="2" />
-                <Die n="6" />
-                <Die n="5" />
-                <Die n="4" />
-                <Die n="2" />
-                <Die n="2" />
-                <Die n="3" />
-                <Die n="2" />
-                <Die n="5" />  
+                {diceElements} 
             </div>
-            <button className="roll-dice">Roll</button>
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
         </main>
   )
 }
